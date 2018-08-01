9485d7099c8ef8e7663346ae5a32a25482b39a34
Tyler Hill <tyler@abstract.kr>
2018-08-01 08:58:53 -0500

    Added getting active game.

---
+++ activegame.go
@@ -0,0 +1,45 @@
+package ggriot
+
+// ActiveGame is the struct that holds information about the current game the summoner is in.
+type ActiveGame struct {
+	GameID            int64  `json:"gameId"`
+	GameStartTime     int    `json:"gameStartTime"`
+	PlatformID        string `json:"platformId"`
+	GameMode          string `json:"gameMode"`
+	MapID             int    `json:"mapId"`
+	GameType          string `json:"gameType"`
+	GameQueueConfigID int    `json:"gameQueueConfigId"`
+	Observers         struct {
+		EncryptionKey string `json:"encryptionKey"`
+	} `json:"observers"`
+	Participants []struct {
+		ProfileIconID            int           `json:"profileIconId"`
+		ChampionID               int           `json:"championId"`
+		SummonerName             string        `json:"summonerName"`
+		GameCustomizationObjects []interface{} `json:"gameCustomizationObjects"`
+		Bot                      bool          `json:"bot"`
+		Perks                    struct {
+			PerkStyle    int   `json:"perkStyle"`
+			PerkIds      []int `json:"perkIds"`
+			PerkSubStyle int   `json:"perkSubStyle"`
+		} `json:"perks"`
+		Spell2ID   int `json:"spell2Id"`
+		TeamID     int `json:"teamId"`
+		Spell1ID   int `json:"spell1Id"`
+		SummonerID int `json:"summonerId"`
+	} `json:"participants"`
+	GameLength      int `json:"gameLength"`
+	BannedChampions []struct {
+		TeamID     int `json:"teamId"`
+		ChampionID int `json:"championId"`
+		PickTurn   int `json:"pickTurn"`
+	} `json:"bannedChampions"`
+}
+
+func GetActiveGame(region string, summoner string) (err error, game *ActiveGame) {
+	err = apiRequest(region+"."+Base+BaseSpectator+"/active-games/by-summoner/"+summoner+apikey, game)
+	if err != nil {
+		return
+	}
+	return
+}
---
+++ ggriot.go
@@ -0,0 +1,101 @@
+package ggriot
+
+import (
+	"net/http"
+	"github.com/pkg/errors"
+	"github.com/json-iterator/go"
+	"io/ioutil"
+)
+
+var (
+	// apikey is APIkey for Riot's API. Not exported to prevent leaks.
+	apikey string
+
+	// Base is the base url w/o a server.
+	Base = "api.riotgames.com/lol"
+
+	// BaseMastery is the path for mastery api calls.
+	BaseMastery = "/champion-mastery/v3"
+
+	// BaseChampion is the path for champion api calls.
+	BaseChampion = "/platform/v3/champions"
+
+	// BaseLeague is the path for ranked leagues api calls.
+	BaseLeague = "/league/v3"
+
+	// BaseStatus is the path for service status api calls.
+	BaseStatus = "/status/v3/shard-data"
+
+	// BaseMatch is the path for match data api calls.
+	BaseMatch = "/match/v3"
+
+	// BaseSpectator is the path for spectator api calls.
+	BaseSpectator = "/spectator/v3"
+
+	// BaseSummoner is the path for summoner information api calls.
+	BaseSummoner = "/summoner/v3"
+
+	// BaseTPC is the path for using third party codes to verify summoners via api calls.
+	BaseTPC = "/platform/v3/third-party-code/by-summoner"
+
+	// BaseTournamentS is the path for using tournament functions via api calls.
+	// This is using the STUB version.
+	BaseTournamentS = "/tournament-stub/v3"
+
+	// BaseTournament is the path for using tournament functions via api calls.
+	// This can only be used with a production API key.
+	BaseTournament = "/tournament/v3"
+
+	// RU is the server code for the Russian servers.
+	RU = "RU"
+	// KR is the server code for the South Korean servers.
+	KR = "KR"
+	// BR is the server code for the Brazil servers.
+	BR = "BR1"
+	// OC is the server code for the Oceania servers.
+	OC = "OC1"
+	// JP is the server code for the Japan servers.
+	JP = "JP1"
+	// NA is the server code for the North America servers.
+	NA = "NA1"
+	// EUN is the server code for the European Union North servers.
+	EUN = "EUN1"
+	// EUW is the server code for the European Union West servers.
+	EUW = "EUW1"
+	// TR is the server code for the Turkish servers.
+	TR = "TR1"
+	// LA1 is the server code for the first Latin America server.
+	LA1 = "LA1"
+	// LA2 is the server code for the second Latin America server.
+	LA2 = "LA2"
+)
+
+// SetAPIKey will set the api key for the global session.
+func SetAPIKey(key string) {
+	// apikey is the global api key.
+	apikey = "?api_key="+key
+}
+
+func apiRequest(request string, s interface{}) (err error){
+
+	req, err := http.Get(request)
+	if err != nil {
+		return errors.New("error requesting, "+err.Error())
+	}
+	defer req.Body.Close()
+
+	if req.StatusCode != http.StatusOK {
+		return // Add api codes here
+	}
+
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		return // Add return error here.
+	}
+	err = jsoniter.Unmarshal(body, s)
+	if err != nil {
+		return
+	}
+}
+
+
